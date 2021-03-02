94ae313bc03fb0d570a0f9c6bd331937247d589e
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-03-02 16:01:20 +0900

    ルーティング

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+// components
+import { Restaurants } from './containers/Restaurants.jsx';
+import { Foods } from './containers/Foods.jsx';
+import { Orders } from './containers/Orders.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          テキストを変更してみる
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <Switch>
+        // 店舗一覧ページ
+        <Route
+          exact
+          path="/restaurants">
+          <Restaurants />
+        </Route>
+        // フード一覧ページ
+        <Route
+          exact
+          path="/foods"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Foods />
+        </Route>
+        // 注文ページ
+        <Route
+          exact
+          path="/orders">
+          <Orders />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+// propsとは親・子コンポーネント間でのデータの「受け渡し口」のようなもの。
+// 親コンポーネントから渡したいデータをpopsという箱に入れ、子コンポーネントもpropsという箱を受け取れるようにし、子コンポーネントの中でpropsの箱の中のデータを参照します。
+
+//
