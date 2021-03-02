68de0d90406e1cf2a55243cc615f7667ed544b7f
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-03-02 12:06:02 +0900

    indexアクションの追加

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -2,6 +2,26 @@ module Api
   module V1
     class LineFoodsController < ApplicationController
       before_action :set_food, only: %i[create] #onlyオプションをつけることで、特定のアクションの実行前にだけ追加することができる。
+      
+      def index
+        line_food = LineFood.active #LineFoodモデルの中からactiveなものを取得して、変数に代入している
+        #モデル名.scope名という形でactive: trueなLineFoodの一覧がActiveRecord_Relationの形で取得
+        if line_foods.exists? #.existsメソッドは対象のインスタンスのデータがDBに存在するかどうかの確認
+          render json: { #正常パターンとしてデータを返す
+            line_food_ids: line_foods.map { |line_food| line_food.id },
+            #登録されているLineFoodのidを配列型式にしています。Rubyの.mapメソッドは配列やハッシュオブジェクトなどを一つずつ取り出し、.mapより後ろのブッロクを当てている
+            restaurant: line_foods[0].restaurant,
+            #一つの仮注文につき一つの店舗という仕様のため、line_foodの中にある先頭のline_foodインスタンスの店舗の情報を詰めています。
+            count: line_fods.sum { |line_food| line_food[:count] },
+            #各line_foodインスタンスには数量を表す:countがあります。フロントエンドにはそれぞれの数量を返して、フロント側で合算するという方法もある。
+            amount: line_foods.sum { |line_food| line_food.total_amount }, 
+            #最後のamountには各line_foodがインスタンスメソッドtotal_amountを呼んで、またその数値を合算している。これは「数量*単価」のさらに合計を計算している。
+          }, status: :ok
+        else
+          render json: {}, status: :no_content
+          #空データがJSON形式で返されます。
+        end
+      end
 
       def create
         if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
