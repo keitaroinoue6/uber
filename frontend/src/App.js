34cb0c9e82845c6efcb424bc6722e5cd13371c43
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-03-02 22:12:47 +0900

    foodsのルーティングの変更

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
         // 店舗一覧ページ
         <Route
           exact
+          // exactというpropsはデフォルトではfalseになっており、ここをtrueにすると明示しなくても、exactとするだけでok
           path="/restaurants">
           <Restaurants />
         </Route>
@@ -34,6 +35,15 @@ function App() {
           path="/orders">
           <Orders />
         </Route>
+        <Route
+          exact
+          path="/restaurants/:restaurantsId/foods" //パラメータとして設定したい部分は:paramNameと: をつけます
+          render={({ match }) => //コンポーネントにmatchというpropsを渡しながら、設定したPATHに対応するリクエストがあった場合にパラメーターと一緒にコンポーネントをレンダリングします
+            <Foods
+              match={match}
+            />
+          }
+          />
       </Switch>
     </Router>
   );
@@ -44,4 +54,14 @@ export default App;
 // propsとは親・子コンポーネント間でのデータの「受け渡し口」のようなもの。
 // 親コンポーネントから渡したいデータをpopsという箱に入れ、子コンポーネントもpropsという箱を受け取れるようにし、子コンポーネントの中でpropsの箱の中のデータを参照します。
 
-//
+// propsにはふたつ書き方がある
+// export const ChildComponent = (props) => {
+//   // (1) props.idの方法で取得する方法
+//   const hoge = () => { props.id }
+// }
+
+// export const ChildComponent = ({ id }) => {
+//   // (2) idと直接key名を指定してデータを参照する方法
+//   const hoge = () => { id }
+// }
+
