4fa5e7e718d8540fc161afbe11c315435ffa0e7c
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-03-03 14:05:22 +0900

    cors.rbの変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001'
+    # ここで、originを文字列で設定しています。正規表現にマッチするoriginを指定することもできます。
+
+    resource '*', # そして、どのようなHTTPリクエストを許可するのか?をここで設定できます。
+                  # 例えばGETだけは許可する、みたいなことがここでは制限できます。今回はいったん全て許可していますが、もし本番環境で必要のないメソッドがあれば追加しなほうが安全です。
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
