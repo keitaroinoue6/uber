e2c63ce230863f52d90d92c3c59f6bef44f67240
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-03-04 14:01:49 +0900

    Restaurants.jsxにスタイルの追加

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,19 +1,57 @@
 import React, { Fragment, useEffect } from 'react';
+import styled from 'styled-components'; //このようにすることでstyled-componentsを使うことができる
+
+// apis
 import { fetchRestaurants } from '../apis/restaurants';
+
+// images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div` 
+
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+//styyled.divでdivElementsに対してスタイルを当てています。ここではページの一番上部にあるヘッダー全体を定義しています。
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+//style.imgのようにすることで、imgElementsに対してもスタイルを当てることができます。
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
 
 export const Restaurants = () => {
   useEffect(() => {
     fetchRestaurants()
     .then((data) =>
       console.log(data)
-      )
-  },[])
+    )
+  }, [])
+
   return (
     <Fragment>
-      レストラン一覧
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
     </Fragment>
   )
 }
+
+
+
+
 
 //useEffect自体をimport { useEffect } from 'react'; このようにimportします。
 //今回はコンポーネントのレンダリング時に一度だけ実行したいので、useEffectの第二引数に空配列[]を入れている。
